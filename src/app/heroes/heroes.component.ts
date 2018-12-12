01f37960586f7be6745aacd85406bcbcf1218761
Tianqi Ren <tqren@thoughtworks.com>
2018-12-12 13:13:12 +0800

    [colin] add function update hero and add hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,9 @@ export class HeroesComponent implements OnInit {
 
   constructor(private heroService: HeroService) {
   }
+
   heroes: Hero[];
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
@@ -19,6 +21,16 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroes();
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 
 }
